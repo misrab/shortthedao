6910730cf12122a6d3d03b656adbbe7946c0049b
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2016-05-20 15:13:35 +0800

    commit

--- sol/daoswap.sol
+++ sol/daoswap.sol
@@ -71,19 +71,20 @@ contract DaoSwap {
     if (msg.sender != owner) { throw; }
 
     // backward indexing for FIFO
-    _index_sellers = sellers.length - 1;
-    _index_buyers = buyers.length - 1;
-
-    while (_index_sellers > 0 && _index_buyers > 0) {
+    _index_sellers = 0; //sellers.length - 1;
+    _index_buyers = 0; // buyers.length - 1;
+    bool success;
+    while (_index_sellers < sellers.length && _index_buyers < buyers.length) {
       // case 1: buyer and seller values match
-      bool success;
       if (sellers[_index_sellers].number_tokens == buyers[_index_buyers].number_tokens) {
+        // for this to work, this contract must have been authorised for said amount
         success = TheDao.transferFrom(sellers[_index_sellers].addr, buyers[_index_buyers].addr, buyers[_index_buyers].number_tokens);
         if (success) {
+          // return seller's deposit, and transfer ether revenue to them
           sendSeller(sellers[_index_sellers].addr, sellers[_index_sellers].number_tokens, true);
-          _index_buyers--;
+          _index_buyers++;
         }
-        _index_sellers--; // decrement regardless
+        _index_sellers++; // decrement regardless
 
       // case 2: buyer > seller
       // note we're always sending the min number_tokens between seller and buyer
@@ -94,7 +95,7 @@ contract DaoSwap {
           buyers[_index_buyers].number_tokens -= sellers[_index_sellers].number_tokens;
           // don't decrement buyer regardless - they have more to buy
         }
-        _index_sellers--;
+        _index_sellers++;
 
       // case 3: buyer < seller
       } else {
@@ -103,23 +104,19 @@ contract DaoSwap {
           // first refund the amount so far
           sellers[_index_sellers].number_tokens -= buyers[_index_buyers].number_tokens;
           sendSeller(sellers[_index_sellers].addr, buyers[_index_buyers].number_tokens, false);
-          _index_buyers--;
+          _index_buyers++;
         } else {
           // only decrement if we're skipping them because they failed
-          _index_sellers--;
+          _index_sellers++;
         }
       }
     }
 
     // clear remaining buyers or sellers
-    if (_index_sellers > 0) { reimburseRemaining(sellers, _index_sellers); }
-    if (_index_buyers > 0) { reimburseRemaining(buyers, _index_buyers); }
+    if (_index_sellers < sellers.length) { reimburseRemaining(sellers, _index_sellers); }
+    if (_index_buyers < buyers.length) { reimburseRemaining(buyers, _index_buyers); }
   }
   // matches a buyer to a seller
-  /*function match(Account seller, Account buyer) {
-    // try to transfer said amount
-    bool success = TheDao.transferFrom(seller.addr, buyer.addr, )
-  }*/
   function sendSeller(address addr, uint number_tokens, bool sendDepositBool) {
     uint amount = number_tokens * SINGLE_TOKEN_PRICE_IN_FINNEY * WEI_PER_FINNEY;
     if (sendDepositBool) { amount *= 2; }
@@ -134,8 +131,7 @@ contract DaoSwap {
   */
   function reimburseRemaining(Account[] accounts, uint index) {
     // first sellers then buyers
-    // <= !
-    for(uint i = 0; i <= index; i++) {
+    for(uint i = index; i < accounts.length; i++) {
       a[i].send(a[i].number_tokens * SINGLE_TOKEN_PRICE_IN_FINNEY * WEI_PER_FINNEY);
     }
   }
